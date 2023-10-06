c61912a413fac850208611795fb6a40c454e3b5b
zontchan <semenovajuls@icloud.com>
2023-10-06 15:45:25 +0700

    some changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 import Chat from "./Chat";
 const container = document.querySelector('.container');
-const url = 'ws://192.168.31.48:7070/ws';
+const url = 'ws://127.0.0.1:7070/ws';
 const chat = new Chat(container, url);
 
